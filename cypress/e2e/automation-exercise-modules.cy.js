db6c3500b82b61a011c71d9847e7fd20d2cad538
wmocelin-qa <wesleymocelin@gmail.com>
2025-10-30 17:35:15 -0300

    criada modularizacao e feito diversas refatoracoes para melhor visualizaçao

--- cypress/e2e/automation-exercise-modules.cy.js
+++ cypress/e2e/automation-exercise-modules.cy.js
@@ -1,9 +1,9 @@
 /// <reference types="cypress" />
 
-import { getTimeStamp, getRandomEmail, getRandomNumber } from '../support/helpers.js'
-import { faker } from '@faker-js/faker';
-import menu from '../modules/login/index.js'; 
-import { preencherCadastroPreLogin, preencherFormularioCadastro  } from '../modules/cadastro/index.js';
+import menu from '../modules/menu/index.js'; 
+import { preencherCadastroPreLogin, preencherFormularioCadastro, realizarCadastroComDadosExistentes } from '../modules/cadastro/index.js';
+import { realizarEnvioFormContato } from '../modules/contato/index.js'
+import { loginUsuarioSenhaCorreto, loginUsuarioSenhaIncorreto, realizarLogoutUsuario } from '../modules/login/index.js'
 import 'cypress-mochawesome-reporter/register';
 
 /* 
@@ -20,56 +20,29 @@ describe('Automation Exercise', () => {
         cy.visit('https://automationexercise.com/')
         menu.navegarParaLogin()
     })
-    it.only('Cadastrar um usuário', () => {
+
+    it('Registrar usuário com sucesso', () => {
         preencherCadastroPreLogin()
         preencherFormularioCadastro()
-    });
+    })
 
     it('Login de Usuário com e-mail e senha corretos', () => {
-        cy.get('[data-qa="login-email"]').type('qatester-1759531045838@test.com.br')
-        cy.get('[data-qa="login-password"]').type('12345')
-        cy.get('[data-qa="login-button"]').click()
-        cy.contains('Logged in as QA Tester Wesley').should('be.visible')
+        loginUsuarioSenhaCorreto()
     })
 
     it('Login de Usuário com e-mail e senha incorretos', () => {
-        cy.get('[data-qa="login-email"]').type('qatester-1759531045838@test.com.br')
-        cy.get('[data-qa="login-password"]').type('1234d5')
-        cy.get('[data-qa="login-button"]').click()
-        cy.contains('Your email or password is incorrect').should('be.visible')
+        loginUsuarioSenhaIncorreto()
     })
 
     it('Logout de usuário', () => {
-        cy.get('[data-qa="login-email"]').type('qatester-1759531045838@test.com.br')
-        cy.get('[data-qa="login-password"]').type('12345')
-        cy.get('[data-qa="login-button"]').click()
-        cy.contains('Logout').should('be.visible').click()
-        cy.url().should('contain', '/login')
+        realizarLogoutUsuario()
     })
 
     it('Cadastrar um usuário existente', () => {
-        cy.get('[data-qa="signup-name"]').type('QA Tester Wesley')
-        cy.get('[data-qa="signup-email"]').type(`qatester-1759531045838@test.com.br`)
-        cy.contains('button', 'Signup').click()
-        cy.contains('Email Address already exist!').should('be.visible')
+        realizarCadastroComDadosExistentes()
     });
 
-    it('Exemplos de Logs', () => {
-        cy.log(`PGTAS Automação Web`)
-        cy.log(`getTimeStamp: ${getTimeStamp()}`)
-        cy.log(`getRandomNumber: ${getRandomEmail()}`)
-        cy.log(`FirstName: ${faker.person.firstName()}`)
-        cy.log(`Random Number: ${getRandomNumber()}`)
-    })
-
     it('Envio de formulário com arquivo "Contact US"', () => {
-        cy.get('a[href="/contact_us"]').click()
-        cy.get('[data-qa="name"]').type('João da Silva')
-        cy.get('[data-qa="email"]').type('joaodasilva.teste@teste.com.br')
-        cy.get('[data-qa="subject"]').type('testando com cypress')
-        cy.get('[data-qa="message"]').type('my message - test')
-        cy.get('input[type="file"][name="upload_file"]').selectFile('cypress/fixtures/example.json')
-        cy.get('[data-qa="submit-button"]').click()
-        cy.contains('Success! Your details have been submitted successfully.').should('be.visible')
+       realizarEnvioFormContato()
     })
 });
--- cypress/modules/login/index.js
+++ cypress/modules/login/index.js
@@ -1,11 +1,21 @@
-class Menu {
-  navegarParaLogin() {
-    cy.get('a[href="/login"]').click()
+export function loginUsuarioSenhaCorreto() {
+    cy.get('[data-qa="login-email"]').type('qatester-1759531045838@test.com.br')
+    cy.get('[data-qa="login-password"]').type('1234d5')
+    cy.get('[data-qa="login-button"]').click()
+    cy.contains('Your email or password is incorrect').should('be.visible')
   }
 
-  efetuarLogout() {
-    cy.get('a[href="/logout"]').should('be.visible').click()
+export function loginUsuarioSenhaIncorreto () {
+    cy.get('[data-qa="login-email"]').type('qatester-1759531045838@test.com.br')
+    cy.get('[data-qa="login-password"]').type('1234d5')
+    cy.get('[data-qa="login-button"]').click()
+    cy.contains('Your email or password is incorrect').should('be.visible')
   }
+
+export function realizarLogoutUsuario () {
+  cy.get('[data-qa="login-email"]').type('qatester-1759531045838@test.com.br')
+  cy.get('[data-qa="login-password"]').type('12345')
+  cy.get('[data-qa="login-button"]').click()
+  cy.get('a[href="/logout"]').click()
+  cy.url().should('contain', '/login')
 }
-
-export default new Menu()
